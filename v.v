d433b1a4b98eef23a3e3a8e02251b4418b0a096e
Gera Weiss <gera.weiss@gmail.com>
2020-06-16 11:16:15 +0300

    Added new features
    This is identical to the code the code that we used to generate the file `v` that you can run, except for the `**********` in the flag.

--- v.v
+++ v.v
@@ -1,9 +1,13 @@
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <regex>
 
 using namespace std;
+
+#define flag "CSCML2020{**********}\n"
+string run(string cmd);
 
 /***********************************************************************************************
 /* This is a compiler of a simple c-like language called v. 
@@ -22,13 +26,28 @@ int main(int argc, char *argv[])
 	code = regex_replace(code, regex("([^\\\\])\\\\xy"), string("$1\xy"));
 	code = regex_replace(code, regex("([^\\\\])\\\\hw"), string("$1\hw"));
 	code = regex_replace(code, regex("([^\\\\])\\\\yx"), string("$1\yx"));
-	code = regex_replace(code, regex("([^\\\\])\\\\\\*"), string("$1if(!run(dest)"));
-
-	cout << code << endl;
-
+	code = regex_replace(code, regex("([^\\\\])\\\\\\*"), string("$1\*"));\*.compare(code)) cout<<\yx\xy;
+	
 	string cmd = "g++ -std=c++11 -o" + dest + " -xc++ -";
 
 	FILE *in = popen(cmd.c_str(), "w");
-	fwrite(code.c_str(), sizeof(char), code.length(), in);
+    fwrite(code.c_str(), sizeof(char), code.length(), in);
 	pclose(in);
 }
+
+string run(string cmd) {
+    string data;
+    FILE * stream;
+    const int max_buffer = 256;
+    char buffer[max_buffer];
+	cmd.append(" 2>&1");
+	
+    stream = popen(cmd.c_str(), "r");
+    if (stream) {
+      while (!feof(stream))
+        if (fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
+          pclose(stream);
+      }
+    return data;
+}
+
